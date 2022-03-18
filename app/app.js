3bcacaaf56efec9ef66a9c6d30cadbdd801cd97d
Narendra Singh <narendra.bokaro@gmail.com>
2022-03-19 01:39:03 +0530

    code clean up

--- app/app.js
+++ app/app.js
@@ -10,15 +10,10 @@ const routes = [
 ]
 
 const router = VueRouter.createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: VueRouter.createWebHashHistory(),
-    routes, // short for `routes: routes`
+    routes
 })
 
-// 5. Create and mount the root instance.
 const app = Vue.createApp({})
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
 app.use(router);
-
 app.mount('#app');
